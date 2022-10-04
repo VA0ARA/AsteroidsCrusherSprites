e69d5470c22922f06b0b257fb94f1d2f6fe8c709
vahid ara <vahidaara@gmail.com>
2022-10-04 17:45:18 +0330

    limit a player for move

--- ShipConteroler.cs
+++ ShipConteroler.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ShipConteroler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public float _Speed = 0.01f;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
+        // you can not intiallize variable in unity directly you need to use vectore3 var for intiallize them...
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.position += new Vector3( h,v, 0f)*Time.deltaTime;
+        if (transform.position.x < -8.12f)
+        {
+            Vector3 SaveDataOfMinNegspos= transform.position;
+            SaveDataOfMinNegspos.x = -8.12f;
+            transform.position = SaveDataOfMinNegspos;
+        }
+        if (transform.position.x > 8.17f)
+        {
+            Vector3 SaveDataOfMaxPosespos = transform.position;
+            SaveDataOfMaxPosespos.x = 8.17f;
+            transform.position = SaveDataOfMaxPosespos;
+        }
+
+
         
     }
-}
+}//class

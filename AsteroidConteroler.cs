c3dce4e8178413ab31489f70af0eeb792c3bfb9f
vahid ara <vahidaara@gmail.com>
2022-10-05 18:18:43 +0330

    add AsteroidSpawner class

--- AsteroidConteroler.cs
+++ AsteroidConteroler.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class AsteroidConteroler : MonoBehaviour
 {
     #region public variabels
+    public float Speed;
+    public float RotationSpeed;
     #endregion
     #region private variabels 
     #endregion
     #region public Method
     #endregion
     #region private Method 
+    private void Update()
+    {
+        // move a long a Y axes
+        transform.position += Vector3.down * Speed * Time.deltaTime;
+        //roatate a long a Z axes
+        transform.Rotate(Vector3.forward * RotationSpeed * Time.deltaTime);
+    }
     #endregion
 
 }
